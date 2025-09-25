6dbf4c564fcc365c38207e8657b5a6767073c82c
tobyrushton <tobyjrushton@icloud.com>
2025-09-25 18:42:42 +0100

    proxy: add basic proxy server functionality

--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -21,8 +21,39 @@ func NewProxy(port int) *Proxy {
 
 func (p *Proxy) Start() error {
 	portString := fmt.Sprintf(":%d", p.Port)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-	})
+	http.HandleFunc("/", p.handleRequest)
 	return http.ListenAndServe(portString, nil)
+}
+
+func (p *Proxy) handleRequest(w http.ResponseWriter, r *http.Request) {
+	res, found := p.checkCache(r.URL.Path)
+	if found {
+		res.Header.Add("X-Cache", "HIT")
+		res.Write(w)
+		return
+	} else {
+		res, err := p.makeRequest(r)
+		if err != nil {
+			http.Error(w, "error making request", http.StatusInternalServerError)
+			return
+		}
+		res.Header.Add("X-Cache", "MISS")
+		res.Write(w)
+		return
+	}
+}
+
+func (p *Proxy) checkCache(key string) (*http.Response, bool) {
+	return p.Cache.Get(key)
+}
+
+func (p *Proxy) makeRequest(r *http.Request) (*http.Response, error) {
+	client := &http.Client{}
+	res, err := client.Do(r)
+	if err != nil {
+		return nil, err
+	}
+
+	p.Cache.Set(r.URL.Path, *res)
+	return res, nil
 }
